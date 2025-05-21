4e3e1b10cbaf235c5f0385e49e0a6395bcefc929
Kumar <mkumar5@allegion.com>
2025-05-20 23:20:38 -0400

    test

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,24 @@
 import { Component, Input} from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
 
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
 
-  @Input() avatar
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+
+  get imagePath() {
+
+    return 'assets/users/' + this.avatar;
+
+  }
+  onSelectUser(){}
+
 }
 
 
